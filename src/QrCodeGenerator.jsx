c47cadc6ee52f70257cbd2db8766272e5cb1c9d6
Denis <mr.den.saint@yandex.ru>
2024-08-20 17:21:28 +0300

    add styles

--- src/QrCodeGenerator.jsx
+++ src/QrCodeGenerator.jsx
@@ -1,20 +1,35 @@
-import {QRCodeSVG} from 'qrcode.react';
-import { useState } from 'react';
+import { QRCodeSVG } from "qrcode.react";
+import { useState } from "react";
+import "./qrCodeGenerator.css";
 
 export const QrCodeGenerator = () => {
-    const [value, setValue] = useState('');
-    const onClickHandler = (event) => {
-        console.log(value);
-    }
-    const onChangeHandler = (event) => {
-        setValue(event.target.value)
-    }
-    return (
-    <>
-        <QRCodeSVG value="Привет мир!!!" />
-        <input type="text" value={value} onChange={onChangeHandler}/>
-        <button type='button' onClick={onClickHandler}>Сгенерировать</button>
-    </>
-    )
-    
-}
+  const [value, setValue] = useState("");
+  const [valueQr, setValueQr] = useState("");
+  const [isShowQR, setIsShowQR] = useState(false);
+  const onClickHandler = (event) => {
+    setIsShowQR(true);
+    setValueQr(value);
+    setValue("");
+  };
+  const onChangeHandler = (event) => {
+    setValue(event.target.value);
+  };
+  return (
+    <div className="content">
+      <h1 className="title">Сгенерировать QR код</h1>
+      <div className="prompt">
+        <input
+            type="text"
+            value={value}
+            onChange={onChangeHandler}
+            className="input"
+            placeholder="Введите текст ..."
+        />
+        <button type="button" onClick={onClickHandler} className="button">
+            Сгенерировать
+        </button>
+      </div>
+      {isShowQR && <QRCodeSVG value={valueQr} />}
+    </div>
+  );
+};
